deca714328119209f5f4848a778454c4c0d23978
Raul Bazan <Raul.BazanSanchez@gmail.com>
2025-07-20 12:30:24 -0400

    Converted wind direction, sunset/sunrise times to readable metrics

--- clean_data.py
+++ clean_data.py
@@ -16,9 +16,96 @@ df['temperature_2m_mean'] = df['temperature_2m_mean'].round(2)
 
 
 
-print(df.isna().sum())
+
 
 
 df = df.dropna()
 
-print(df.isna().sum())
+# Codes for weather code
+# =============================================================================
+# clearSky = 0
+# mainlyClear = 1
+# partlyCloudy = 2
+# overcast = 3
+# fog = 45
+# depositingRimeFog = 48
+# lightDrizzle = 51
+# moderateDrizzle = 53
+# denseDrizzle = 55
+# lightFreezingDrizzle = 56
+# moderateOrDenseFreezingDrizzle = 57
+# lightRain = 61
+# moderateRain = 63
+# heavyRain = 65
+# lightFreezingRain = 66
+# moderateOrHeavyFreezingRain = 67
+# slightSnowfall = 71
+# moderateSnowfall = 73
+# heavySnowfall = 75
+# snowGrains = 77
+# slightRainShowers = 80
+# moderateRainShowers = 81
+# heavyRainShowers = 82
+# slightSnowShowers = 85
+# heavySnowShowers = 86
+# thunderstormSlightOrModerate = 95
+# thunderstormStrong = 96
+# thunderstormHeavy = 99
+# =============================================================================
+
+df['weather_code'] = df['weather_code'].astype(int)
+
+weather_map_code = {
+    0 : "clearSky",
+    1 : "mainlyClear",
+    2 : "partlyCloudy",
+    3 : "overcast",
+    45 : "fog",
+    48 : "depositingRimeFog",
+    51 : "lightDrizzle",
+    53 : "moderateDrizzle",
+    55 : "denseDrizzle",
+    56 : "lightFreezingDrizzle",
+    57 : "moderateOrDenseFreezingDrizzle",
+    61 : "lightRain",
+    63 : "moderateRain",
+    65 : "heavyRain",
+    66 : "lightFreezingRain",
+    67 : "moderateOrHeavyFreezingRain",
+    71 : "slightSnowfall",
+    73 : "moderateSnowfall",
+    75 : "heavySnowfall",
+    77 : "snowGrains",
+    80 : "slightRainShowers",
+    81 : "moderateRainShowers",
+    82 : "heavyRainShowers",
+    85 : "slightSnowShowers",
+    86 : "heavySnowShowers",
+    95 : "thunderstormSlightOrModerate",
+    96 : "thunderstormStrong",
+    99 : "thunderstormHeavy"
+
+}
+
+
+df['weather_code'] = df['weather_code'].map(weather_map_code)
+
+def compassConvert(degree):
+    directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    index = int((degree + 22.5) % 360 / 45)
+    return directions[index]
+
+df['wind_direction'] = df['wind_direction_10m_dominant'].apply(compassConvert)
+
+#print(df.dtypes)
+
+
+df['date'] = pd.to_datetime(df['date']).dt.date
+#df['date'] = pd.to_datetime(df['date'], format= '%Y%m%d')
+df['date'] = pd.to_datetime(df['date'])
+df['sunriseTime'] = pd.to_datetime(df['sunrise'], unit='s')
+df['sunriseTime'] = df['sunriseTime'] - pd.Timedelta(hours=4)
+df['sunsetTime'] = pd.to_datetime(df['sunset'], unit='s')
+df['sunsetTime'] = df['sunsetTime'] - pd.Timedelta(hours=4)
+print(df.dtypes)
+
