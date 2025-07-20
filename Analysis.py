beeb524972b65027318740d3460add0c33b53d87
Raul Bazan <Raul.BazanSanchez@gmail.com>
2025-07-20 15:25:14 -0400

    Playing with the Graphs

--- Analysis.py
+++ Analysis.py
@@ -21,20 +21,36 @@ seasonal_temp = df.groupby(["year","season"] )["temperature_2m_max"].mean().rese
 seasonal_temp = seasonal_temp.sort_values(["season", "year"])
 seasonal_temp["pct_change"] = seasonal_temp.groupby("season")["temperature_2m_max"].pct_change() * 100
 
+print('seasonal temp')
+print(seasonal_temp.dtypes)
+
+
 
 print(seasonal_temp)
 
 #FeelsLikeTemp = df.groupby(["year","season"])["apparent_temperature_mean"].mean().reset_index()
-grouped = df.groupby(["year", "season"])["temperature_2m_max"].mean().reset_index()
+maxTemp = df.groupby(["year", "season"])["temperature_2m_max"].mean().reset_index()
 
 # 3. Plot
-pivot_df = grouped.pivot(index="year", columns="season", values="temperature_2m_max")
+# pivot_df = maxTemp.pivot(index="year", columns="season", values="temperature_2m_max")
 
-# Plot
-pivot_df.plot(marker='o', figsize=(10, 6))
-plt.title("Seasonal Apparent Temperature Trends Over Years")
+# # Plot
+# pivot_df.plot(marker='o', figsize=(10, 6))
+# plt.title("Seasonal Apparent Temperature Trends Over Years")
+# plt.xlabel("Year")
+# plt.ylabel("Avg Apparent Temperature (°F)")
+# plt.grid(True)
+# plt.legend(title="Season")
+# plt.tight_layout()
+# plt.show()
+
+
+seasonal = seasonal_temp.pivot(index="year", columns="season", values="pct_change")
+
+seasonal.plot(kind='bar', figsize=(10,6))
+plt.title("Percentage Change in Average Max Temps")
 plt.xlabel("Year")
-plt.ylabel("Avg Apparent Temperature (°F)")
+plt.ylabel("Percentage change")
 plt.grid(True)
 plt.legend(title="Season")
 plt.tight_layout()
